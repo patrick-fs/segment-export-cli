238560f01220928581b1b3ecb2f6a66d16d6b8de
Patrick Brandt <patrick@fullstory.com>
2020-04-07 17:27:16 -0400

    adding api functions

---
+++ src/api.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import config from './config';
+
+export interface ExportOptions {
+  segment_id: string,
+  type: string,
+  format: string,
+  time_range: {
+    start: string,
+    end: string,
+  }
+}
+
+export interface OperationResponse {
+  state: string,
+  estimatePctComplete: number,
+  results: {
+    searchExportId: string,
+    expires: string
+  }
+}
+
+const initApi = ({ apiKey, baseURL = 'https://api.staging.fullstory.com'} : { apiKey: string, baseURL?: string}) => {
+  return axios.create({
+    baseURL: baseURL,
+    timeout: 1000,
+    headers: {
+      'Authorization': `Basic ${apiKey}`,
+      'Content-Type': 'application/json',
+    }
+  });
+}
+
+const api = initApi({apiKey: config().API_KEY});
+
+export const startExport = async (options: ExportOptions) => {
+  const exportResp = await api.post<{operationId: string}>('/segments/v1/exports', options);
+  return exportResp.data;
+};
+
+export const getOperation = async (operationId: string) => {
+  const operationsResp = await api.get<OperationResponse>(`/operations/v1/${operationId}`);
+  return operationsResp.data;
+};
+
+export const getExportFileURL = async (exportId: string) => {
+  const downloadResp =  await api.get<{location: string, expires: string}>(`/search/v1/exports/${exportId}/results`);
+  return downloadResp.data;
+}
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export default () : { API_KEY: string } => {
+  const currentFileUrl = path.resolve('.config');
+  const rawJson = fs.readFileSync(path.join(currentFileUrl, 'fullstory.json'), { encoding:'utf-8'});
+  return JSON.parse(rawJson);
+}
