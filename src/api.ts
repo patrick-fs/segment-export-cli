1755d418d5d0a93e88b323c0c80f1a4320f2b460
Patrick Brandt <patrick@fullstory.com>
2020-04-08 00:05:29 -0400

    eslint fixup

--- src/api.ts
+++ src/api.ts
@@ -1,28 +1,28 @@
-import axios from 'axios';
-import config from './config';
+import axios from 'axios'
+import config from './config'
 
 export interface ExportOptions {
-  segment_id: string,
-  type: string,
-  format: string,
+  segment_id: string;
+  type: string;
+  format: string;
   time_range: {
-    start: string,
-    end: string,
-  }
+    start: string;
+    end: string;
+  };
 }
 
 export interface OperationResponse {
-  state: string,
-  estimatePctComplete: number,
+  state: string;
+  estimatePctComplete: number;
   results: {
-    searchExportId: string,
-    expires: string
-  }
+    searchExportId: string;
+    expires: string;
+  };
 }
 
 export interface FileUrlResponse {
-  location: string,
-  expires: string,
+  location: string;
+  expires: string;
 }
 
 export enum ExportTypes {
@@ -35,34 +35,34 @@ export enum ExportFormats {
   csv = 'FORMAT_CSV'
 }
 
-const initApi = ({ apiKey, baseURL = 'https://api.staging.fullstory.com'} : { apiKey: string, baseURL?: string}) => {
+const initApi = ({apiKey, baseURL = 'https://api.staging.fullstory.com'}: { apiKey: string; baseURL?: string}) => {
   return axios.create({
     baseURL: baseURL,
     timeout: 1000,
     headers: {
-      'Authorization': `Basic ${apiKey}`,
+      Authorization: `Basic ${apiKey}`,
       'Content-Type': 'application/json',
-    }
-  });
+    },
+  })
 }
 
-const api = initApi({apiKey: config().API_KEY});
+const api = initApi({apiKey: config().API_KEY})
 
 export const startExport = async (options: ExportOptions) => {
-  const exportResp = await api.post<{operationId: string}>('/segments/v1/exports', options);
-  return exportResp.data;
-};
+  const exportResp = await api.post<{operationId: string}>('/segments/v1/exports', options)
+  return exportResp.data
+}
 
 export const getOperation = async (operationId: string) => {
-  const operationsResp = await api.get<OperationResponse>(`/operations/v1/${operationId}`);
-  return operationsResp.data;
-};
+  const operationsResp = await api.get<OperationResponse>(`/operations/v1/${operationId}`)
+  return operationsResp.data
+}
 
 export const getExportFileURL = async (exportId: string) => {
-  const downloadResp =  await api.get<FileUrlResponse>(`/search/v1/exports/${exportId}/results`);
-  return downloadResp.data;
-};
+  const downloadResp =  await api.get<FileUrlResponse>(`/search/v1/exports/${exportId}/results`)
+  return downloadResp.data
+}
 
 export const getExportFileStream = async (fileUrl: FileUrlResponse) => {
-  return await axios.get(fileUrl.location, { responseType: 'stream' });
-};
+  return axios.get(fileUrl.location, {responseType: 'stream'})
+}
--- src/commands/export.ts
+++ src/commands/export.ts
@@ -1,146 +1,157 @@
-import * as fs from 'fs';
-import { DateTime, Interval, Duration } from 'luxon';
-import * as mkdirp from 'mkdirp';
-import { Command, flags } from '@oclif/command';
-import * as ora from 'ora';
-import * as zlib from 'zlib';
-import * as fsApi from '../api';
+import * as fs from 'fs'
+import {DateTime, Interval, Duration} from 'luxon'
+import * as mkdirp from 'mkdirp'
+import {Command, flags} from '@oclif/command'
+import * as ora from 'ora'
+import * as zlib from 'zlib'
+import * as fsApi from '../api'
 
-const BATCH_SIZE = 2;
-const DATA_DIRECTORY = './data';
+const BATCH_SIZE = 2
+const DATA_DIRECTORY = './data'
 
 export default class GetSegment extends Command {
   static args = [
-    { 
+    {
       name: 'id',
       required: true,
-      description: 'segment id of the segment you created in FullStory'
+      description: 'segment id of the segment you created in FullStory',
     },
   ];
 
   static flags = {
-    help: flags.help({char: 'h'}),    
-    start: flags.string({char: 's', description: 'start of query: mm/dd/yyyy', required: true }),
-    end: flags.string({char: 'e', description: 'end of query: mm/dd/yyyy' }),
-    format: flags.string({ char: 'f', options: ['JSON', 'CSV'], default: 'FORMAT_CSV', parse: i => {
-      if (i === 'JSON') return fsApi.ExportFormats.json;
-      return fsApi.ExportFormats.csv;
+    help: flags.help({char: 'h'}),
+    start: flags.string({char: 's', description: 'start of query: mm/dd/yyyy', required: true}),
+    end: flags.string({char: 'e', description: 'end of query: mm/dd/yyyy'}),
+    format: flags.string({char: 'f', options: ['JSON', 'CSV'], default: 'FORMAT_CSV', parse: i => {
+      if (i === 'JSON') return fsApi.ExportFormats.json
+      return fsApi.ExportFormats.csv
     }}),
-    type: flags.string({ char: 't', options: ['event', 'individual'], default: fsApi.ExportTypes.event, parse: i => {
-      if (i === 'event') return fsApi.ExportTypes.event;
-      return fsApi.ExportTypes.individual;
+    type: flags.string({char: 't', options: ['event', 'individual'], default: fsApi.ExportTypes.event, parse: i => {
+      if (i === 'event') return fsApi.ExportTypes.event
+      return fsApi.ExportTypes.individual
     }}),
-    directory: flags.string({char: 'd', description: 'location of the output directory', default: DATA_DIRECTORY }),
+    directory: flags.string({char: 'd', description: 'location of the output directory', default: DATA_DIRECTORY}),
   };
 
   async run() {
-    const { args, flags } = this.parse(GetSegment);
+    const {args, flags} = this.parse(GetSegment)
     // TODO: validate flags and args formats
     // or choose a default start...
-    
-    const intervals = this.getIntervals(flags.start, flags.end);
-    let downloads: Promise<any>[] = [];
 
-    const spinner = ora('Getting export').start();
-    spinner.color = 'yellow';
+    const intervals = this.getIntervals(flags.start, flags.end)
+    let downloads: Promise<any>[] = []
+
+    const spinner = ora('Getting export').start()
+    spinner.color = 'yellow'
 
     const exportOptions = {
       segment_id: args.id,
       type: flags.type,
       format: flags.format,
     }
-    
+
     for (let i = 0; i < intervals.length; i++) {
       try {
-        downloads.push(this.downloadFile(intervals[i], exportOptions, flags.directory));
+        downloads.push(this.downloadFile(intervals[i], exportOptions, flags.directory))
         if (i > 0 && i % BATCH_SIZE === 0) {
-          await Promise.all(downloads);
-          spinner.text = `downloaded: ${i}/${intervals.length}`;
-          downloads = [];
+          await Promise.all(downloads) // eslint-disable-line no-await-in-loop
+          spinner.text = `downloaded: ${i}/${intervals.length}`
+          downloads = []
         }
-      } catch (e) {
-        console.log(`failed to download files: ${e}`);
-        downloads = [];
+      } catch (error) {
+        console.error(`failed to download files: ${error}`)
+        downloads = []
       }
-      
     }
 
     spinner.stopAndPersist({
       text: '✅ Segment export complete!',
-    });
+    })
   }
 
   getIntervals(startDate: string, endDate?: string, intervalDuration = '15') {
-    const segmentDuration = Duration.fromISO(`PT${intervalDuration}M`);
-    const start = DateTime.fromFormat(startDate, 'D');
-    let end: DateTime;
+    const segmentDuration = Duration.fromISO(`PT${intervalDuration}M`)
+    const start = DateTime.fromFormat(startDate, 'D')
+    let end: DateTime
     if (endDate && endDate !== '') {
-      end = DateTime.fromFormat(endDate, 'D');
+      end = DateTime.fromFormat(endDate, 'D')
     } else {
-      const { day, month, year } = DateTime.fromMillis(Date.now()).toObject();
-      end = DateTime.fromObject({ day, month, year });
+      const {day, month, year} = DateTime.fromMillis(Date.now()).toObject()
+      end = DateTime.fromObject({day, month, year})
     }
-    const interval = Interval.fromDateTimes(start, end);
-    return interval.splitBy(segmentDuration);
+    const interval = Interval.fromDateTimes(start, end)
+    return interval.splitBy(segmentDuration)
   }
 
   async downloadFile<T extends Omit<fsApi.ExportOptions, 'time_range'>>(interval: Interval, exportOptions: T, directory = DATA_DIRECTORY) {
-    const { day, month, year } = interval.start.toObject();
-    const directoryName = `${directory}/${exportOptions.segment_id}/${this.getExportType(exportOptions.type)}/${year}-${month}-${day}`;
-    mkdirp(directoryName);
+    const {day, month, year} = interval.start.toObject()
+    const directoryName = `${directory}/${exportOptions.segment_id}/${this.getExportType(exportOptions.type)}/${year}-${month}-${day}`
+    mkdirp(directoryName)
 
-    const start = interval.start.toISO( { includeOffset: false });
-    const end = interval.end.toISO( { includeOffset: false })
+    const start = interval.start.toISO({includeOffset: false})
+    const end = interval.end.toISO({includeOffset: false})
 
     const exportResp = await fsApi.startExport({
       ...exportOptions,
       time_range: {
         start: `${start}Z`,
         end: `${end}Z`,
-      }
-    }).catch(e => { console.error(`failed to start export: ${e}`); throw e });
+      },
+    }).catch(error => {
+      console.error(`failed to start export: ${error}`)
+      throw error
+    })
 
     const opComplete = await this.pollUntilComplete(exportResp.operationId)
-      .catch(e => { console.error(`failed to poll operations: ${e}`); throw e });
+    .catch(error => {
+      console.error(`failed to poll operations: ${error}`)
+      throw error
+    })
     const fileUrl = await fsApi.getExportFileURL(opComplete.searchExportId)
-      .catch(e => { console.error(`failed to get export file url: ${e}`); throw e });
+    .catch(error => {
+      console.error(`failed to get export file url: ${error}`)
+      throw error
+    })
     const fileStream = await fsApi.getExportFileStream(fileUrl)
-      .catch(e => { console.error(`failed to get export file stream: ${e}`); throw e });
+    .catch(error => {
+      console.error(`failed to get export file stream: ${error}`)
+      throw error
+    })
     const writeStream = fs.createWriteStream(
       `${directoryName}/${start}.${this.getFileExtension(exportOptions.format)}`,
-      { encoding: 'utf-8' });
-    
-    const unzip = zlib.createGunzip();
-    fileStream.data.pipe(unzip).pipe(writeStream);
+      {encoding: 'utf-8'})
+
+    const unzip = zlib.createGunzip()
+    fileStream.data.pipe(unzip).pipe(writeStream)
   }
 
   getFileExtension(format: string) {
-    if (format === fsApi.ExportFormats.json) return 'json';
-    return 'csv';
+    if (format === fsApi.ExportFormats.json) return 'json'
+    return 'csv'
   }
 
   getExportType(exportOptionType: string) {
-    if (exportOptionType === fsApi.ExportTypes.event) return 'event';
-    return 'individual';
+    if (exportOptionType === fsApi.ExportTypes.event) return 'event'
+    return 'individual'
   }
 
-  pollUntilComplete(operationId: string): Promise<{searchExportId: string, expires: string}> {
+  pollUntilComplete(operationId: string): Promise<{searchExportId: string; expires: string}> {
     return new Promise((resolve, reject) => {
-      let operationsResp: fsApi.OperationResponse;
+      let operationsResp: fsApi.OperationResponse
       const id = setInterval(async () => {
         try {
-          operationsResp = await fsApi.getOperation(operationId);
-        } catch (err) {
-          return reject(`failed to get operation: ${err}`);
+          operationsResp = await fsApi.getOperation(operationId)
+        } catch (error) {
+          return reject(new Error(`failed to get operation: ${error}`))
         }
         if (operationsResp.state === 'COMPLETED') {
-          clearInterval(id);
+          clearInterval(id)
           resolve({
             searchExportId: operationsResp.results.searchExportId,
             expires: operationsResp.results.expires,
           })
         }
-      }, 2000);
-    });
+      }, 2000)
+    })
   }
 }
